48d799f0c594e9d6bdcd4532c446312e90d444c3
James Craig <craig.jamesa@gmail.com>
2020-06-07 19:45:54 -0400

    Package updates. Added a method to the tables to be able to convert it automatically to a class specified.

--- test/FileCurator.Tests/Formats/Excel/ExcelReaderTests.cs
+++ test/FileCurator.Tests/Formats/Excel/ExcelReaderTests.cs
@@ -8,6 +8,19 @@ namespace FileCurator.Tests.Formats.Excel
     public class ExcelReaderTests : TestingDirectoryFixture
     {
         [Fact]
+        public void Convert()
+        {
+            var TestObject = new ExcelReader();
+            var Result = TestObject.Read(File.OpenRead("../../../TestData/TestXLSX.xlsx"));
+            var Results = Result.Convert<ExcelTestData>();
+            Assert.Equal(2, Results.Count);
+            Assert.Equal("Goes", Results[0].Test);
+            Assert.Equal("here", Results[0].Data);
+            Assert.Equal(string.Empty, Results[1].Test);
+            Assert.Equal("1", Results[1].Data);
+        }
+
+        [Fact]
         public void Read()
         {
             var TestObject = new ExcelReader();
@@ -19,6 +32,13 @@ namespace FileCurator.Tests.Formats.Excel
             Assert.Equal("Goes", Result.Rows[0].Cells[0].Content);
             Assert.Equal("here", Result.Rows[0].Cells[1].Content);
             Assert.Equal("1", Result.Rows[1].Cells[1].Content);
+            Assert.Equal(1, Result.Rows[1].Cells[1].GetValue<int>());
+        }
+
+        public class ExcelTestData
+        {
+            public string Data { get; set; }
+            public string Test { get; set; }
         }
     }
 }
